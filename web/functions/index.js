b0b40bb856cc07e820616bc19254b18c8ff5b6a8
jonghyo <m.73816686@gmail.com>
2019-03-16 18:27:37 +0900

    add User Landlist autoupdate functions

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,8 +1,15 @@
-const functions = require('firebase-functions');
+const funcs = {
+  userLandUpdate: './src/firebaseUserLandUpdater',
+};
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const loadFunctions = (funcsObj) => {
+  for (const name in funcsObj) {
+    if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === name) {
+      exports[name] = require(funcsObj[name]);
+    }
+  }
+};
+
+console.log('process.env.FUNCTION_NAME:', process.env.FUNCTION_NAME);
+loadFunctions(funcs);
+console.log('exports:', exports);
